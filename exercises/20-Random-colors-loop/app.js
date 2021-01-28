02fb360dc85ee768224cbc68b0db84db37074240
Mauriel <josueazofeifajosue@gmail.com>
2021-01-28 16:53:43 +0000

    #20 ready

--- exercises/20-Random-colors-loop/app.js
+++ exercises/20-Random-colors-loop/app.js
@@ -1,26 +1,29 @@
-function getColor(colorNumber=0)
-{
-	//make sure parameter is a number and not a string by converting the value to int:
-	colorNumber = parseInt(colorNumber);
-	switch(colorNumber){
-		case 1: return "red"; 
-				break;
-		case 2: return "yellow"; 
-				break;
-		case 3: return "blue"; 
-				break;
-		case 4: return "green"; 
-				break;
-		default: return "black"; 
-				 break;
-	}
+function getColor(colorNumber = 0) {
+    //make sure parameter is a number and not a string by converting the value to int:
+    colorNumber = parseInt(colorNumber);
+    switch (colorNumber) {
+        case 1: return "red";
+            break;
+        case 2: return "yellow";
+            break;
+        case 3: return "blue";
+            break;
+        case 4: return "green";
+            break;
+        default: return "black";
+            break;
+    }
 }
 
-function getAllStudentColors(){
-	
-	//your loop here
-	var exampleColor = getColor(1);
+function getAllStudentColors() {
+    for (var i = 1; i <= 10; i++) {
+        var colorRandom = Math.floor((Math.random() * 4) + 1);
+        console.log(getColor(colorRandom));
+    }
 }
+//
+//Math.floor retorna el numero mas cercano inferior
+//Math.ceil retorna el numero mas cercano superior
 
 //call the function below with the number of students in the class and print on the console
 getAllStudentColors();
